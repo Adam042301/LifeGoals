5e40ba56f617ada4203e2e5967d4688135c3fe45
Adamkelly <Adamkelly0104@gmail.com>
2025-08-05 08:53:47 -0400

    removing mobile touch

--- slideShow.js
+++ slideShow.js
@@ -23,7 +23,7 @@ function plusSlides(n) {
   clearTimeout(slideTimer);  // Reset timer on manual control
   slideIndex += n;
   showSlides(slideIndex);
-  slideTimer = setTimeout(autoSlide, 5000); // Restart auto-slide
+  slideTimer = setTimeout(autoSlide, 6000); // Restart auto-slide
 }
 
 // Thumbnail image controls
@@ -31,7 +31,7 @@ function currentSlide(n) {
   clearTimeout(slideTimer);
   slideIndex = n;
   showSlides(slideIndex);
-  slideTimer = setTimeout(autoSlide, 5000);
+  slideTimer = setTimeout(autoSlide, 6000);
 }
 
 // Automatic slideshow
@@ -39,40 +39,40 @@ function autoSlide() {
   slideIndex++;
   if (slideIndex > slides.length) { slideIndex = 1; }
   showSlides(slideIndex);
-  slideTimer = setTimeout(autoSlide, 5000);
+  slideTimer = setTimeout(autoSlide, 6000);
 }
 
 // Initialize slideshow
 showSlides(slideIndex);
-slideTimer = setTimeout(autoSlide, 5000);
+slideTimer = setTimeout(autoSlide, 6000);
 
 
 
 
 // Swipe Gesture Support
-let touchStartX = 0;
-let touchEndX = 0;
+// let touchStartX = 0;
+// let touchEndX = 0;
 
-const slideshowContainer = document.querySelector('.slideshow-container');
+// const slideshowContainer = document.querySelector('.slideshow-container');
 
-slideshowContainer.addEventListener('touchstart', function(e) {
-  touchStartX = e.changedTouches[0].screenX;
-}, false);
+// slideshowContainer.addEventListener('touchstart', function(e) {
+//   touchStartX = e.changedTouches[0].screenX;
+// }, false);
 
-slideshowContainer.addEventListener('touchend', function(e) {
-  touchEndX = e.changedTouches[0].screenX;
-  handleGesture();
-}, false);
+// slideshowContainer.addEventListener('touchend', function(e) {
+//   touchEndX = e.changedTouches[0].screenX;
+//   handleGesture();
+// }, false);
 
-function handleGesture() {
-  const swipeThreshold = 50; // Minimum distance in px to be considered a swipe
-  if (touchEndX < touchStartX - swipeThreshold) {
-    plusSlides(1); // Swipe Left -> Next Slide
-  }
-  if (touchEndX > touchStartX + swipeThreshold) {
-    plusSlides(-1); // Swipe Right -> Previous Slide
-  }
-}
+// function handleGesture() {
+//   const swipeThreshold = 50; // Minimum distance in px to be considered a swipe
+//   if (touchEndX < touchStartX - swipeThreshold) {
+//     plusSlides(1); // Swipe Left -> Next Slide
+//   }
+//   if (touchEndX > touchStartX + swipeThreshold) {
+//     plusSlides(-1); // Swipe Right -> Previous Slide
+//   }
+// }
 
 
 // Pause on hover (desktop)
@@ -89,62 +89,5 @@ function pauseSlideshow() {
 }
 
 function resumeSlideshow() {
-  slideTimer = setTimeout(autoSlide, 5000);
+  slideTimer = setTimeout(autoSlide, 6000);
 }
-
-
-
-
-
-
-
-// let slideIndex = 0;
-// let slides = document.getElementsByClassName("mySlides");
-// let dots = document.getElementsByClassName("dot");
-
-// function showSlides() {
-//   for (let i = 0; i < slides.length; i++) {
-//     slides[i].classList.remove("active");
-//   }
-//   for (let i = 0; i < dots.length; i++) {
-//     dots[i].className = dots[i].className.replace(" active", "");
-//   }
-
-//   slideIndex++;
-//   if (slideIndex > slides.length) { slideIndex = 1; }
-
-//   slides[slideIndex - 1].classList.add("active");
-//   dots[slideIndex - 1].className += " active";
-
-//   setTimeout(showSlides, 6000); // Change image every 5 seconds
-// }
-
-// showSlides();
-
-
-
-
-
-
-
-
-
-// let slideIndex = 0;
-// showSlides();
-
-// function showSlides() {
-//   let i;
-//   let slides = document.getElementsByClassName("mySlides");
-//   let dots = document.getElementsByClassName("dot");
-//   for (i = 0; i < slides.length; i++) {
-//     slides[i].style.display = "none";  
-//   }
-//   slideIndex++;
-//   if (slideIndex > slides.length) {slideIndex = 1}    
-//   for (i = 0; i < dots.length; i++) {
-//     dots[i].className = dots[i].className.replace(" active", "");
-//   }
-//   slides[slideIndex-1].style.display = "block";  
-//   dots[slideIndex-1].className += " active";
-//   setTimeout(showSlides, 3500); // Change image every 2 seconds
-// }
